2129f071fe6e06c59e27f3be7c839dc69c1e5e82
charescape <charescape@outlook.com>
2020-10-28 17:24:04 +0800

    Create RESTControllerTrait.php

---
+++ src/RESTControllerTrait.php
@@ -0,0 +1,45 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Zii\Util;
+
+use Yii;
+use yii\data\ActiveDataProvider;
+use yii\data\Pagination;
+use yii\data\Sort;
+use yii\db\ActiveQueryInterface;
+
+trait RESTControllerTrait
+{
+    use ControllerTrait;
+
+    private function getActiveDataProviderConfig(ActiveQueryInterface $query, array $query_sort = []): ActiveDataProvider
+    {
+        if (empty($query_sort)) {
+            $query_sort = ['id' => SORT_DESC];
+        }
+
+        /** @var ActiveDataProvider $providerConfig */
+        $providerConfig = Yii::createObject([
+            'class' => ActiveDataProvider::class,
+            'query' => $query,
+            'pagination' => [
+                'class' => Pagination::class,
+                'params' => Yii::$app->getRequest()->getQueryParams(),
+                'pageParam' => 'qpage',
+                'pageSizeParam' => 'qpagesize',
+                'pageSizeLimit' => [1, 20],
+            ],
+            'sort' => [
+                'class' => Sort::class,
+                'params' => Yii::$app->getRequest()->getQueryParams(),
+                'sortParam' => 'qsort',
+                'enableMultiSort' => true,
+                'defaultOrder' => $query_sort,
+            ],
+        ]);
+
+        return $providerConfig;
+    }
+}
