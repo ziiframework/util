06465f011a3d672f09d51f765d6b6a4c35676b01
charescape <charescape@outlook.com>
2020-10-30 11:52:53 +0800

    Update StatelessControllerTrait.php

--- src/StatelessControllerTrait.php
+++ src/StatelessControllerTrait.php
@@ -7,10 +7,6 @@ namespace Zii\Util;
 use Yii;
 use yii\base\Model;
 use yii\base\UnknownClassException;
-use yii\data\ActiveDataProvider;
-use yii\data\Pagination;
-use yii\data\Sort;
-use yii\db\ActiveQueryInterface;
 use yii\filters\AccessControl;
 use yii\filters\Cors;
 use yii\helpers\Inflector;
@@ -67,9 +63,11 @@ trait StatelessControllerTrait
 
         // make sure put `authenticator` behavior after `cors` behavior
         // see issue: https://github.com/yiisoft/yii2/issues/14754
-        $original_authenticator_settings = $behaviors['authenticator'];
-        unset($behaviors['authenticator']);
-        $behaviors['authenticator'] = $original_authenticator_settings;
+        if (isset($behaviors['authenticator'])) {
+            $original_authenticator_settings = $behaviors['authenticator'];
+            unset($behaviors['authenticator']);
+            $behaviors['authenticator'] = $original_authenticator_settings;
+        }
 
         $behaviors['authenticator']['authMethods'] = $this->resolveAuthorizationMethods();
         $behaviors['authenticator']['except'] = ['options'];
@@ -98,10 +96,6 @@ trait StatelessControllerTrait
             return false;
         }
 
-        if (!isset($this->actions()[$action->id])) {
-            $this->checkAccess($action->id);
-        }
-
         $this->prepareValidatorObject();
         if (($this->validatorObject instanceof Model) && (!$this->validatorObject->validate())) {
             Yii::$app->response->data = ['err' => $this->validatorObject->errors];
@@ -109,62 +103,5 @@ trait StatelessControllerTrait
         }
 
         return true;
-    }
-
-    public function actions(): array
-    {
-        $actions = parent::actions();
-
-        /** @see IndexAction::$dataFilter */
-        $actions['index']['dataFilter'] = $this->dataFilterCallback();
-
-        /** @see IndexAction::$prepareDataProvider */
-        $actions['index']['prepareDataProvider'] = $this->prepareDataProviderCallback();
-
-        return $actions;
-    }
-
-    protected function dataFilterCallback(): ?array
-    {
-        return null;
-    }
-
-    protected function prepareDataProviderCallback(): ?callable
-    {
-        return null;
-    }
-
-    public function checkAccess($action, $model = null, $params = []): void
-    {
-        parent::checkAccess($action, $model, $params);
-    }
-
-    protected function getActiveDataProviderConfig(ActiveQueryInterface $query, array $query_sort = []): ActiveDataProvider
-    {
-        if (empty($query_sort)) {
-            $query_sort = ['id' => SORT_DESC];
-        }
-
-        /** @var ActiveDataProvider $providerConfig */
-        $providerConfig = Yii::createObject([
-            'class' => ActiveDataProvider::class,
-            'query' => $query,
-            'pagination' => [
-                'class' => Pagination::class,
-                'params' => Yii::$app->getRequest()->getQueryParams(),
-                'pageParam' => 'qpage',
-                'pageSizeParam' => 'qpagesize',
-                'pageSizeLimit' => [1, 20],
-            ],
-            'sort' => [
-                'class' => Sort::class,
-                'params' => Yii::$app->getRequest()->getQueryParams(),
-                'sortParam' => 'qsort',
-                'enableMultiSort' => true,
-                'defaultOrder' => $query_sort,
-            ],
-        ]);
-
-        return $providerConfig;
     }
 }
