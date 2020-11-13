c37ecf46853dbdb9aa3b0fca560a7539f31d1681
charescape <charescape@outlook.com>
2020-11-13 23:34:55 +0800

    Create XmlParser.php

---
+++ src/XmlParser.php
@@ -0,0 +1,25 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Zii\Util;
+
+use Symfony\Component\Serializer\Encoder\XmlEncoder;
+use Symfony\Component\Serializer\Serializer;
+use yii\base\BaseObject;
+use yii\web\RequestParserInterface;
+
+class XmlParser extends BaseObject implements RequestParserInterface
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function parse($rawBody, $contentType): array
+    {
+        $xml_encoder = new XmlEncoder([XmlEncoder::ROOT_NODE_NAME => 'xml', XmlEncoder::ENCODING => 'UTF-8']);
+
+        $serializer = (new Serializer([], [$xml_encoder]));
+
+        return $serializer->decode($rawBody,'xml');
+    }
+}
