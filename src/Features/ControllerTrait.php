72838457e510d527109d258837b323238776175f
charescape <charescape@outlook.com>
2020-12-10 14:26:08 +0800

    Features

--- src/Features/ControllerTrait.php
+++ src/Features/ControllerTrait.php
@@ -2,7 +2,7 @@
 
 declare(strict_types=1);
 
-namespace Zii\Util;
+namespace Zii\Util\Features;
 
 use yii\base\InvalidConfigException;
 use yii\base\Model;
--- src/Features/TableControllerTrait.php
+++ src/Features/TableControllerTrait.php
@@ -2,7 +2,7 @@
 
 declare(strict_types=1);
 
-namespace Zii\Util;
+namespace Zii\Util\Features;
 
 use Yii;
 use yii\data\ActiveDataProvider;
