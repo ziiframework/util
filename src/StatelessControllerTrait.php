71bd234bdc15f5b4be2110c8e6679f6e6a80a55c
charescape <charescape@outlook.com>
2021-01-27 15:13:20 +0800

    Update StatelessControllerTrait.php

--- src/StatelessControllerTrait.php
+++ src/StatelessControllerTrait.php
@@ -32,10 +32,9 @@ trait StatelessControllerTrait
             /** @var Model $validatorObject */
             $this->validatorObject = $validatorObject;
             $this->validatorObject->setScenario(Inflector::id2camel($php_url_part[3]));
+            $this->validatorObject->setAttributes(Yii::$app->getRequest()->get());
             if (Yii::$app->getRequest()->getIsPost()) {
                 $this->validatorObject->setAttributes(Yii::$app->getRequest()->post());
-            } else {
-                $this->validatorObject->setAttributes(Yii::$app->getRequest()->get());
             }
         }
     }
